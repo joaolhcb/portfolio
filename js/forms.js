95a5681f249db019b3746e9b4240a69b4462cbd0
João Barboza <joao.lhcb@gmail.com>
2022-06-13 19:16:16 -0300

    Enviar formulário

--- js/forms.js
+++ js/forms.js
@@ -1,23 +1,16 @@
-var arr = [];
-
+//Enviar formulário
 function sendMessage(){
-    if (localStorage.meuArr){             
-        arr = JSON.parse(localStorage.getItem('meuArr')); 
-     }
-     
-     let name = document.getElementById("name").value;
-     let sobrenome = document.getElementById("sobrenome").value;
-     let email = document.getElementById("email").value;
-     let tel = document.getElementById("tel").value;
-     let mensagem = document.getElementById("mensagem").value;
-     arr.push(name, sobrenome, email, tel, mensagem);
-     document.getElementById("name").value = "";
-     document.getElementById("sobrenome").value = "";
-     document.getElementById("email").value = "";
-     document.getElementById("tel").value = "";
-     document.getElementById("mensagem").value = "";
-     localStorage.meuArr = JSON.stringify(arr);
+    if (typeof (Storage) !== "undefined") {
+
+        if (localStorage.cont) {
+            localStorage.cont = Number(localStorage.cont) + 1;
+        } else {
+            localStorage.cont = 1;
+        }
+    cad = document.getElementById('name').value + ';' + document.getElementById('sobrenome').value + ';' + document.getElementById('email').value + ';' + document.getElementById('tel').value + ';' + document.getElementById('mensagem').value;
+    localStorage.setItem("cad" + localStorage.cont, cad);
     }
+}
 
 //Limpar Local Storage
 function cleanMessage(){
